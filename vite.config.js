976e9f30f72e17d65dcd45920f842ff7ff95c284
DeanNab175 <f.jscript@gmail.com>
2022-12-05 17:10:53 +0400

    remove docs folder + on vite configs

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const outDir = resolve(__dirname, 'docs')
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir
-  }
+  plugins: [react()]
 })
